cc96ed57bf3277ea7bb0a269327939187bf75b54
First Last <salmanashraf853@gmail.com>
2019-10-12 21:11:46 +0100

    connecting to database

--- server.js
+++ server.js
@@ -16,41 +16,10 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-
-
-
-const users = [
-    {
-        username: 'Ashraf',
-        fullName: 'Salman Ashraf',
-        age: '18',
-        phone: 08169094945,
-        gender: 'male',
-        regDate: '17-sep-19',
-        id: 'NG303935'
-    },
-    {
-        username: 'Daniel',
-        regDate: '19-sep-19',
-        fullName: 'Samuel Daniel',
-        phone: 08168354675,
-        age: '28',
-        gender: 'male',
-        id: 'NG335965'
-    },
-    {
-        username: 'Mary',
-        fullName: 'Olaiwola Mary',
-        phone: 08165894385,
-        regDate: '27-sep-19',
-        age: '20',
-        gender: 'female',
-        id: 'NG204756'
-    },
-]
-
 app.get('/users', (req, res)=>{
-    res.json(users)
+    db.select('*').from('users').then(data => {
+        res.json(data)
+    })
 })
 
 app.listen(process.env.PORT)
